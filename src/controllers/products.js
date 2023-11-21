796c3ac6e2f42057b8cad01faf0bad2dc549388c
techNinja20 <olawaleolupo@gmail.com>
2023-11-21 18:39:04 +0100

    feat: get single product created and update product adjusted

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,59 +1,77 @@
 const { findQuery, updateOne } = require("../repository")
 const { isEmpty } = require("../utils")
-const {PRODUCT_CATEGORY_ID} = require("../enums/products")
-
-
+const { PRODUCT_CATEGORY_ID } = require("../enums/products")
 
 const getAllProducts = async (req, res, next) => {
+  try {
+    const products = await findQuery("Products", {})
 
-    try {
+    const liveStock = products.filter(
+      (product) =>
+        product.category_id == PRODUCT_CATEGORY_ID.livestock_category_id
+    )
+    const farmProduce = products.filter(
+      (product) =>
+        product.category_id == PRODUCT_CATEGORY_ID.farmProduct_category_id
+    )
 
+    res.status(201).json({
+      status: true,
+      message: "Products fetched successfully ",
+      products: {
+        liveStock: liveStock,
+        farmPrduce: farmProduce,
+      },
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+const getSingleProduct = async (req, res, next) => {
+  const { id } = req.params
 
-        const products = await findQuery("Products", {})
-
-        const liveStock = products.filter(product => ( product.category_id == PRODUCT_CATEGORY_ID.livestock_category_id) ) 
-        const farmProduce = products.filter(product => (product.category_id == PRODUCT_CATEGORY_ID.farmProduct_category_id)) 
-
-        res.status(201).json({
-            status: true,
-            message: "Products fetched successfully ",
-            products:{
-                liveStock : liveStock,
-                farmPrduce : farmProduce
-            }
-        })
-        
-    } catch (error) {
-        next(error)
-    }
+  try {
+    const getProduct = await findQuery("Products", { product_id: id })
+    if (isEmpty(getProduct)) {
+      const err = new Error("Product not found")
+      err.status = 400
+      return next(err)
+      }
+      
+      res.status(201).json({
+        status: true,
+        message: "Product fetched successfully",
+        product: getProduct,
+      })
+  } catch (error) {
+    next(error)
+  }
 }
 
 const updateProduct = async (req, res, next) => {
+  const { id } = req.params
+   
 
-    const {id} = req.params
-    const {product_name,product_price,portion_price} = req.body
+  try {
+    const product = await updateOne( "Products", { product_id: id }, req.body  )
 
-    try {
-
-        const product = await updateOne("Products", {product_id: id},  {product_name:product_name, product_price: product_price, portion_price: portion_price})
-
-       if(product.modifiedCount == 0 || product.matchedCount == 0){
-        const err = new Error("Product has not been updated")
-        err.status = 400
-        return next(err)
-       }
-
-        res.status(201).json({
-            status: true,
-            message: "Account updated successfully",
-        })
-        
-    } catch (error) {
-        next(error)
+    if (product.modifiedCount == 0 || product.matchedCount == 0) {
+      const err = new Error("Product has not been updated")
+      err.status = 400
+      return next(err)
     }
+
+    res.status(201).json({
+      status: true,
+      message: "Account updated successfully",
+    })
+  } catch (error) {
+    next(error)
+  }
 }
 
 module.exports = {
-    getAllProducts,
-    updateProduct
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 const {
-    getAllProducts,updateProduct
-}  = require('../controllers/products')
-
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+} = require("../controllers/products")
 
 /**
  * create a new user record
@@ -14,14 +15,62 @@ const {
  *     description: This gets all products and filter due to category id
  *     tags:
  *       - Products
- *     produces:	 
- *       - application/json	 
+ *     produces:
+ *       - application/json
  *     responses:
  *        201:
- *          description: Account created.
+ *          description: All products fetched.
  *        422:
  *          Bad Request
-*/
-router.get('/products/get-products', getAllProducts)
+ */
+router.get("/products/get/products", getAllProducts)
+
+/**
+ * create a new user record
+ * @swagger
+ * /product/get-single-product/:id:
+ *   get:
+ *     summary: get single product
+ *     description: This gets a single product due to product id
+ *     tags:
+ *       - Products
+ *     produces:
+ *       - application/json
+ *     responses:
+ *        201:
+ *          description: Single product fetched.
+ *        422:
+ *          Bad Request
+ */
+router.get("/product/get_single/product/:id", getSingleProduct)
+
+/**
+ * create a new user record
+ * @swagger
+ * /products/update/product/:id:
+ *   patch:
+ *     summary: update single product
+ *     description: This allows admin to update the product using category id
+ *     tags:
+ *       - Products
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: product_name
+ *         in: body
+ *         required: false
+ *       - name: product_price
+ *         in: body
+ *         required: false
+ *       - name: portion_price
+ *         in: body
+ *         required: false
+ *     responses:
+ *        201:
+ *          description: Product updated successfully.
+ *        422:
+ *          Bad Request
+ */
+router.patch("/products/update/product/:id", updateProduct)
 
 module.exports = router
