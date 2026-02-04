5dc566211cc6391019361de6550205a9b7327da0
Oladele Tijani <jaeswave@gmail.com>
2026-02-04 11:51:15 +0100

    ...7

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { initializePayment, verifyPayment } = require("../controllers/payment");
+const authorization = require("../middleware/authorization");
+
 const router = express.Router();
 
 
-router.post("/initialize", initializePayment);
-router.get("/verify/:reference", verifyPayment);
+router.post("/initialize", authorization, initializePayment);
+router.get("/verify/:reference", authorization, verifyPayment);
 
 module.exports = router;
